3dea5b355bd1af76dbaa38dab975fd54da29cd91
Robinson <rotag0812@gmail.com>
2026-03-10 10:47:47 +0100

    Estructura de carpetas del proyecto
    Creacion de estructura de carpetas del proyecto con archivos temporales
    "Creacion.txt" para la implementacion de todas las carpetas al
    repositorio remoto
    Carpetas:
    -Corp : almacenara todo lo relacionado a la empresa Anti-cape
    -Data-Base: almacenara todo lo correspondiente a la base de datos
    -Documentacion: almacenara la documentacion tecnica del proyecto
    -Programacion: almacenara el codigo Java del proyecto

---
+++ Data-Base/esquemas/ddl/Anti-CapeDDBB.sql
@@ -0,0 +1,44 @@
+CREATE TABLE `Cliente` (
+  `id_cliente` integer PRIMARY KEY,
+  `nombre` varchar(255),
+  `colores` varchar(255),
+  `superpoder` varchar(255)
+);
+
+CREATE TABLE `Traje` (
+  `id_traje` integer PRIMARY KEY,
+  `nombre` varchar(255),
+  `estado` varchar(255)
+);
+
+CREATE TABLE `Taller` (
+  `id_taller` integer PRIMARY KEY,
+  `tipo_sala` varchar(255),
+  `nombre_sala` varchar(255)
+);
+
+CREATE TABLE `Empleado` (
+  `id_empleado` integer PRIMARY KEY,
+  `nombre` varchar(255),
+  `apellidos` varchar(255),
+  `apodo` varchar(255),
+  `categoria` varchar(255)
+);
+
+CREATE TABLE `Citas` (
+  `id_cita` integer PRIMARY KEY,
+  `fecha` date,
+  `duracion` timestamp,
+  `id_cliente` integer,
+  `id_empleado` integer,
+  `id_traje` integer,
+  `id_taller` integer
+);
+
+ALTER TABLE `Citas` ADD FOREIGN KEY (`id_cliente`) REFERENCES `Cliente` (`id_cliente`);
+
+ALTER TABLE `Citas` ADD FOREIGN KEY (`id_empleado`) REFERENCES `Empleado` (`id_empleado`);
+
+ALTER TABLE `Citas` ADD FOREIGN KEY (`id_traje`) REFERENCES `Traje` (`id_traje`);
+
+ALTER TABLE `Citas` ADD FOREIGN KEY (`id_taller`) REFERENCES `Taller` (`id_taller`);
