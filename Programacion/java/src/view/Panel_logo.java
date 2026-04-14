5eeb2cb84410eb3f7b626d1ce947c2d47b35fb48
BreixoGarciaCanovacas <breixogarcia490@gmail.com>
2026-04-14 09:06:30 +0200

    Inserción del Logo en el LogIn
    Añado el logo a la izquierda de la pantalla del login

--- Programacion/java/src/view/Panel_logo.java
+++ Programacion/java/src/view/Panel_logo.java
@@ -1,7 +1,9 @@
 package view;
 
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Font;
+import java.awt.Graphics;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
@@ -10,17 +12,30 @@ import javax.swing.border.LineBorder;
 import control.Listener;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.ImageIcon;
 
 public class Panel_logo extends JPanel {
+	
+	ImageIcon ii;
 
 	public Panel_logo(Listener list) {
+		
+		ii = new ImageIcon("src/img/logo.png");
+		
+		
 		setBackground(new Color(240, 240, 240));
 		setLayout(null);
 		setSize(362, 410);
 		
-		JLabel logo = new JLabel("Poner aquí el logo");
-		logo.setHorizontalAlignment(SwingConstants.CENTER);
-		logo.setBounds(127, 102, 108, 32);
-		add(logo);
+
+	}
+	
+	protected void paintComponent(Graphics g) {
+		Dimension dim = this.getSize();
+		
+		g.drawImage(ii.getImage(), 30, 0, 300, 300, null);
+		this.setOpaque(false);
+		
+		super.paintChildren(g);
 	}
 }
